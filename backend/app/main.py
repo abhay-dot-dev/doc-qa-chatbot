64224b23f89382fb27d92352b52c4b8cbe6d725c
Abhay <sabhay3053@gmail.com>
2026-09-25 11:59:08 +0530

    Configured CORS for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # models get imported
 from app.db.database import Base, engine
@@ -12,6 +13,15 @@ from app.api import auth, upload, chat, history
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# middleware to all request from frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(auth.router)
 app.include_router(upload.router)
