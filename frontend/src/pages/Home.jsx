13f27880b71472e6ddb39d6f0e7d6a92761697a4
Abhay <sabhay3053@gmail.com>
2026-09-26 16:39:00 +0530

    Connected conversation history to chat

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import ChatHistorySidebar from '../components/ChatHistorySidebar'
-import PdfUpload from '../components/PdfUpload'
+import { apiRequest } from '../services/api'
+import React, { useState, useEffect } from 'react'
 import ChatBox from '../components/ChatBox'
+import PdfUpload from '../components/PdfUpload'
+import ChatHistorySidebar from '../components/ChatHistorySidebar'
 
 const Home = () => {
+
+  const [selectedConversationId, setSelectedConversationId] = useState(null);
+  const [conversationMessage, setConversationMessage] = useState([]);
+
+
+  async function fetchConversationMessage(conversationId) {
+    const response = await apiRequest(`/history/${conversationId}`, {
+      method: "GET"
+    });
+
+    setConversationMessage(response);
+  }
+
+  useEffect(() => {
+    if (selectedConversationId) {
+      fetchConversationMessage(selectedConversationId);
+    }
+  }, [selectedConversationId]);
+
   return (
     <div>
-        <ChatHistorySidebar/>
-        <PdfUpload />
-        <ChatBox />
+      <ChatHistorySidebar setSelectedConversationId={setSelectedConversationId} />
+      <PdfUpload />
+      <ChatBox
+        conversationMessage={conversationMessage}
+        selectedConversationId={selectedConversationId} />
     </div>
   )
 }
