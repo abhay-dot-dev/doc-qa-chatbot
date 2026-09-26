5e88f9cba40e5930dbb8f55d9a4bd6b1af7421b3
Abhay <sabhay3053@gmail.com>
2026-09-26 17:55:37 +0530

    Moved database session dependency

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -17,3 +17,10 @@ SessionLocal = sessionmaker(
 
 # Base created
 Base = declarative_base()   
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield(db)
+    finally:
+        db.close()
