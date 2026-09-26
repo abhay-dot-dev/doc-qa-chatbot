ba75b9b89d412d6334b169253071ec99f5ced726
Abhay <sabhay3053@gmail.com>
2026-09-26 16:36:54 +0530

    Added conversation message history endpoint

--- backend/app/api/history.py
+++ backend/app/api/history.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import and_
 
 from app.api.auth import get_db
 from app.schemas.history import UserHistory
+from app.models.message import Message
 from app.models.conversation import Conversation
 from app.core.dependencies import get_current_user
 
@@ -18,3 +20,22 @@ def history(current_user = Depends(get_current_user), db : Session = Depends(get
         "user_id": current_user.id,
         "history": conversations
     }
+
+@router.get("/history/{conversation_id}")
+def message(conversation_id: int, current_user = Depends(get_current_user), db: Session = Depends(get_db)):
+    conversation = db.query(Conversation).filter(
+        and_(
+            Conversation.id == conversation_id,
+            Conversation.user_id == current_user.id
+        )
+    ).first()
+
+    if conversation is None:
+        raise HTTPException(
+            status_code=404,
+            detail="The conversation ID doesn't exist"
+        )
+
+    messages = db.query(Message).filter(Message.conversation_id == conversation.id).all()
+
+    return messages
